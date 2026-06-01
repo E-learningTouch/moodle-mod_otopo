bef5f7688dd90149e66e0a1fa070dcdaf9b017b6
Luiggi Sansonetti <1565841+luiggisanso@users.noreply.github.com>
2026-06-01 08:54:31 +0200

    Update version.php

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026042755;
+$plugin->version = 2026052900;
 $plugin->requires = 2022112800; // Requires 5.0.
 $plugin->release = '2.0.1';
 $plugin->supported = [500, 502];
